3435927a15f5d2cc29620469f53fd281d120ae85
TonyGrif <TonyGriffin2000@gmail.com>
2023-04-29 00:57:34 -0400

    add text writer to manager

--- tests/writer/test_writeManager.py
+++ tests/writer/test_writeManager.py
@@ -1,5 +1,6 @@
 import pytest
 from datetime import date
+from pathlib import Path 
 
 from website_analysis.writers.writer import WriteManager
 
@@ -14,6 +15,19 @@ def nonDefWriter():
     yield writer
 
 class TestWriter:
+    def test_write(self, writer, nonDefWriter):
+        writer.write()
+        assert Path(writer.outputDirectory).is_dir()
+        assert Path(writer.outputDirectory + "/" + writer.fileName + ".txt").is_file()
+        Path.unlink(Path(writer.outputDirectory + "/" + writer.fileName + ".txt").resolve())
+        Path.rmdir(Path(writer.outputDirectory).resolve())
+
+        nonDefWriter.write()
+        assert Path(nonDefWriter.outputDirectory).is_dir()
+        assert Path(nonDefWriter.outputDirectory + "/" + nonDefWriter.fileName + ".txt").is_file()
+        Path.unlink(Path(nonDefWriter.outputDirectory + "/" + nonDefWriter.fileName + ".txt").resolve())
+        Path.rmdir(Path(nonDefWriter.outputDirectory).resolve())
+    
     def test_outputDirectory(self, writer, nonDefWriter):
         assert writer.outputDirectory == "output"
 
