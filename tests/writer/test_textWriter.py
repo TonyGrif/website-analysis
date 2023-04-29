cdcbdd3c5f4f881b702f670ec3b68a5d40d5719a
TonyGrif <TonyGriffin2000@gmail.com>
2023-04-29 00:16:52 -0400

    handle output path

--- tests/writer/test_textWriter.py
+++ tests/writer/test_textWriter.py
@@ -1,8 +1,16 @@
 import pytest
+from pathlib import Path
 
 from website_analysis.writers.text_writer import TextWriter
 
+@pytest.fixture
+def tWriter():
+    tWriter = TextWriter("output/test")
+    yield tWriter
+    
 class TestTextWriter:
-    def test_file(self):
-        assert False
+    def test_file(self, tWriter):
+        assert tWriter.fullPath == Path(Path.cwd() / "output/test.txt")
 
+        tWriter.fullPath = "build/output"
+        assert tWriter.fullPath == Path(Path.cwd() / "build/output.txt")
--- website_analysis/writers/text_writer.py
+++ website_analysis/writers/text_writer.py
@@ -1,3 +1,17 @@
+from pathlib import Path 
+
 class TextWriter:
-    def __init__():
-        return
+    def __init__(self, full):
+        self.fullPath = full 
+
+    def write(self):
+        pass
+
+        
+    @property 
+    def fullPath(self) -> Path:
+        return self._fullPath
+
+    @fullPath.setter
+    def fullPath(self, full):
+        self._fullPath = Path(full + ".txt").resolve()
