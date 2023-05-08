35459fb811b78537f5534ed6a7ae66a0b82a9304
TonyGrif <TonyGriffin2000@gmail.com>
2023-05-07 23:32:23 -0400

    Documentation (#14)
    * update tests, add documentation to function
    
    * update tests, add documentation to write manager
    
    * update tests, add documentation to text writer

--- tests/writer/test_textWriter.py
+++ tests/writer/test_textWriter.py
@@ -11,21 +11,25 @@ def tWriter():
 
 
 class TestTextWriter:
+    def test_write(self, tWriter):
+        tWriter.write()
+        assert tWriter.fullPath.parent.exists(), f"Directory path not created"
+        assert tWriter.fullPath.parent.is_dir(), f"Expected directory, found file"
+        assert tWriter.fullPath.exists(), f"File not created"
+        assert tWriter.fullPath.is_file(), f"Expected file, found directory"
+        Path.unlink(tWriter.fullPath)
+
+        tWriter.fullPath = "tests/output/test2"
+        tWriter.write()
+        assert tWriter.fullPath.parent.exists(), f"Directory path not created"
+        assert tWriter.fullPath.parent.is_dir(), f"Expected directory, found file"
+        assert tWriter.fullPath.exists(), f"File not created"
+        assert tWriter.fullPath.is_file(), f"Expected file, found directory"
+        Path.unlink(tWriter.fullPath)
+        Path.rmdir(Path.cwd() / "tests/output")
+
     def test_file(self, tWriter):
         assert tWriter.fullPath == Path(Path.cwd() / "tests/output/test.txt")
 
         tWriter.fullPath = "build/output"
         assert tWriter.fullPath == Path(Path.cwd() / "build/output.txt")
-
-    def test_write(self, tWriter):
-        tWriter.write()
-        assert tWriter.fullPath.parent.is_dir()
-        assert tWriter.fullPath.is_file()
-        Path.unlink(tWriter.fullPath)
-
-        tWriter.fullPath = "tests/output/test2"
-        tWriter.write()
-        assert tWriter.fullPath.parent.is_dir()
-        assert tWriter.fullPath.is_file()
-        Path.unlink(tWriter.fullPath)
-        Path.rmdir(Path.cwd() / "tests/output")
--- website_analysis/utilities.py
+++ website_analysis/utilities.py
@@ -2,6 +2,16 @@ from pathlib import Path
 
 
 def findDirectory(arg: Path) -> bool:
+    """
+    Determine if a given path is a directory.
+
+    Parameters:
+    arg (Path): Given path to analyze.
+
+    Returns:
+    True (bool): If the path exists and is a directory.
+    False (bool): If the path is nonexistent or if the path is not a directory.
+    """
     if not arg.exists():
         print("Not a valid destination")
         return False
