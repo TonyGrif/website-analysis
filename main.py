f35895f016d7d08e0c0a54fccf20ee1fc93cb1ed
TonyGrif <TonyGriffin2000@gmail.com>
2023-09-26 23:16:17 -0400

    add flake8 functionality

--- main.py
+++ main.py
@@ -2,10 +2,11 @@
 
 """The main module for the website-analysis program.
 
-This program allows the user to input a directory containing HTML files and recieve
-output files containing analysis on the data transfer these files would require.
+This program allows the user to input a directory containing HTML
+files and recieve output files containing analysis
+on the data transfer these files would require.
 
-This file can be run as `./main.py` 
+This file can be run as `./main.py`
 """
 
 
