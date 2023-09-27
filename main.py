1488cc61beb946fc3fe3f800155f35267d1551a3
TonyGrif <TonyGriffin2000@gmail.com>
2023-09-26 23:14:01 -0400

    add pylint

--- main.py
+++ main.py
@@ -1,7 +1,16 @@
 #!/usr/bin/python3
 
+"""The main module for the website-analysis program.
+
+This program allows the user to input a directory containing HTML files and recieve
+output files containing analysis on the data transfer these files would require.
+
+This file can be run as `./main.py` 
+"""
+
 
 def main():
+    """Run the website-analysis program."""
     print("Hello World!")
 
 
