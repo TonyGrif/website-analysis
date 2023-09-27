15e45f6a8d78993295fcef6376021993ab8ab0f5
TonyGrif <TonyGriffin2000@gmail.com>
2023-09-26 23:22:03 -0400

    Prelim (#1)
    *  Update README to contain a simple project description and requirements
        Develop a main module from which to launch the program from
        Determine list of initial developer dependencies
        Allow for unit testing

---
+++ main.py
@@ -0,0 +1,19 @@
+#!/usr/bin/python3
+
+"""The main module for the website-analysis program.
+
+This program allows the user to input a directory containing HTML
+files and recieve output files containing analysis
+on the data transfer these files would require.
+
+This file can be run as `./main.py`
+"""
+
+
+def main():
+    """Run the website-analysis program."""
+    print("Hello World!")
+
+
+if __name__ == "__main__":
+    main()
---
+++ tests/test_pytest.py
@@ -0,0 +1,6 @@
+import pytest
+
+
+class TestPytest:
+    def test_true(self):
+        assert True
